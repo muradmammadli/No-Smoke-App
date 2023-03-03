507a44b506af9713c55a48aaf133823d11635e63
Murad <murad.mammadli@yahoo.com>
2023-03-03 17:44:19 +0400

    *

--- app/src/main/java/com/example/nosmokeapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/nosmokeapp/activity/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.nosmokeapp.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.nosmokeapp.R
+import com.example.nosmokeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navhostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navhostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/example/nosmokeapp/activity/StartActivity.kt
+++ app/src/main/java/com/example/nosmokeapp/activity/StartActivity.kt
@@ -1,15 +1,25 @@
 package com.example.nosmokeapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.nosmokeapp.R
+import com.example.nosmokeapp.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(2000)
         installSplashScreen()
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.doneFab.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
     }
 }
