087ade8034ef07f910cbf6b0c0ee45548d5ab608
Murad <murad.mammadli@yahoo.com>
2023-03-06 11:53:24 +0400

    StartActivity.kt created

--- app/src/main/java/com/example/nosmokeapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/nosmokeapp/activity/MainActivity.kt
@@ -2,22 +2,54 @@ package com.example.nosmokeapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.nosmokeapp.R
 import com.example.nosmokeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setNavHostFragment()
 
-        val navhostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navhostFragment.navController
-        binding.bottomNavigationView.setupWithNavController(navController)
 
     }
+
+    private fun setNavHostFragment() {
+        binding.toolbar.setTitleTextColor(getColor(R.color.white))
+        binding.toolbar.title = "İrəliləyiş"
+        val navhostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navhostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.trophiesFragment->{
+                    binding.toolbar.title = "Kuboklar"
+                    true
+                }
+                R.id.healthFragment->{
+                    binding.toolbar.title = "Sağlamlıq"
+                    true
+                }
+                R.id.progressFragment->{
+                    binding.toolbar.title = "İrəliləyiş"
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/nosmokeapp/fragment/ProgressFragment.kt
+++ app/src/main/java/com/example/nosmokeapp/fragment/ProgressFragment.kt
@@ -6,16 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nosmokeapp.R
+import com.example.nosmokeapp.databinding.FragmentProgressBinding
 
 
 class ProgressFragment : Fragment() {
-
+    private lateinit var binding:FragmentProgressBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progress, container, false)
+        var progress = 0
+        binding = FragmentProgressBinding.inflate(inflater,container,false)
+
+        binding.dayProgressBar.progress = progress
+
+
+        binding.button2.setOnClickListener {
+            if (progress <= 90){
+                progress += 10
+                binding.dayProgressBar.progress = progress
+            }
+        }
+
+        binding.button.setOnClickListener {
+            if (progress >= 10){
+                progress -= 10
+                binding.dayProgressBar.progress = progress
+            }
+        }
+
+
+        return binding.root
     }
 
 }
